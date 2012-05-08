a4462aad9444099c05f6fed417a652ac817bc356
Michael Hoffer <info@michaelhoffer.de>
2012-05-08 21:04:33 +0200

    copyright notice updated

--- ndim-neuro/src/edu/gcsc/ndim/neuro/AddNeigboursProcessor.java
+++ ndim-neuro/src/edu/gcsc/ndim/neuro/AddNeigboursProcessor.java
@@ -11,9 +11,33 @@ import org.ndim.GridTopo;
 import org.ndim.MemTopo;
 import org.ndim.Stencil;
 
-/**
+/*
+ * Copyright 2012 Goethe Center for Scientific Computing (G-CSC) All rights reserved.
+ * 
  *
- * @author Michael Hoffer <info@michaelhoffer.de>
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice, this list of
+ *       conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *       of conditions and the following disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Michael Hoffer <info@michaelhoffer.de> "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Michael Hoffer <info@michaelhoffer.de> OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and should not be interpreted as representing official policies, either expressed
+ * or implied, of Goethe Center for Scientific Computing (G-CSC).
  */
 public class AddNeigboursProcessor extends AbstractEntityProcessor {
 
